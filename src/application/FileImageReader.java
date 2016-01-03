e2bbe3a722c62cc79f2f9df1153d12b427005b80
Juan Antonio <Juan Antonio@JuanAntonio-PC>
2016-01-03 23:50:56 +0000

    Sixth Commit

--- src/application/FileImageReader.java
+++ src/application/FileImageReader.java
@@ -1,11 +1,32 @@
 package application;
 
 import java.io.File;
+import java.io.FilenameFilter;
 
 public class FileImageReader {
 
+    private final File[] files;
+    private static final String[] ImageExtensions={".jpg",".png",".gif"};
+    
     public FileImageReader(String path) {
         this(new File(path));
     }
+    
+    public FileImageReader(File folder){
+        this.files = folder.listFiles(withImageExtension());
+    }
+
+    private FilenameFilter withImageExtension() {
+        return new FilenameFilter(){
+            @Override
+            public boolean accept(File dir, String name){
+                for (String extension : ImageExtensions) {
+                    if(name.endsWith(extension))return true;
+                }
+                return false;
+            }
+        };
+    }
+   
     
 }
